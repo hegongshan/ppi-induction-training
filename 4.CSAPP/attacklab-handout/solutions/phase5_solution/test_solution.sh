98d62b31ec6e23c6444f199c18e3777a59113620
hegongshan <hegongshan@qq.com>
2021-06-19 22:50:36 +0800

    optimize:optimize code style

--- 4.CSAPP/attacklab-handout/solutions/phase5_solution/test_solution.sh
+++ 4.CSAPP/attacklab-handout/solutions/phase5_solution/test_solution.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-solution_path_arr=$(ls . | grep -E ".txt$")
+solution_path_arr=`ls . | grep -E ".txt$"`
 pass_count=0
 fail_count=0
 
 for s in ${solution_path_arr[*]}; do
-	result=$(../../hex2raw -i $s |../../rtarget -q | grep -E "PASS")
+	result=`../../hex2raw -i $s |../../rtarget -q | grep -E "PASS"`
 	if [ -n "$result" ]; then
 		pass_count=`expr ${pass_count} + 1`
 	else
@@ -13,4 +13,3 @@ for s in ${solution_path_arr[*]}; do
 done
 
 echo "Number of PASS:${pass_count}, FAILURE: ${fail_count}"
-!
